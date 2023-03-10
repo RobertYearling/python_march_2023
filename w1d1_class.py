71441f98f33de4d5b8fc3707dfed4931cd95dfdf
Rob <rob@askrob.guru>
2023-03-09 18:22:02 -0600

    first upload of all files

---
+++ w1d1_class.py
@@ -0,0 +1,70 @@
+# x = 3
+# print(x)
+
+# if x <= 3:
+#     print(x)
+
+# x = 3
+# print(type(x))
+
+# Booleans
+# is_hungy = True
+# is_not_hungry = False
+# print(type(is_hungy))
+
+# Strings
+# food_truck = "Coding Cow Food Truck"
+# review = 9.97
+# print(len(food_truck))
+# print("The name of the popular food is: ", food_truck)
+# print(f"The name of the popular food truck is: {food_truck}, it has a great review of: {review}")
+
+# x = 3
+# print(type(str(x)))
+
+# List
+# empty = []
+# ninjas = [ 'jayou', 'noe', 38 ]
+# print(ninjas)
+# print(ninjas[2])
+# ninjas[2] = 'francis'
+# print(ninjas)
+# ninjas.append('jeff')
+# ninjas.append('rob')
+# print(ninjas)
+# ninjas.pop(1)
+# print(ninjas)
+# ninjas.pop(5)
+# print(ninjas)
+
+# Tuples
+tuple_data = ('Physics', 'Chemistry', 1997, 2000)
+# print(tuple_data)
+
+# dictionary = {
+#     "Location" : "Oak Park",
+#     "N" : (41,53,16),
+#     "W" : (87,47,59)
+# }
+# print(dictionary)
+
+for i in range(0,41,2):
+    print(i)
+
+# 0 Represents the where to Start
+# 41 Represents where to stop
+# 2 Represents Increment or Decrement, Step
+
+for i in range(0,41):
+    print(i)
+
+# 0 Represents the where to Start
+# 41 Represents where to stop
+# Increment of 1 is implied
+
+for i in range(41):
+    print(i)
+
+# Implies a start of 0
+# 41 Represents where to stop
+# Increment of 1 is implied
